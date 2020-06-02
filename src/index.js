0aa533506581a9ead62d2e3d92d120e3a23d1253
enderpicture <enderpicture@gmail.com>
2020-06-02 01:07:05 -0700

    more formats and quality control

--- src/index.js
+++ src/index.js
@@ -2,32 +2,60 @@ import { main } from 'magica'
 import { File } from 'magica'
 import "./main.scss"
 
-console.log('done loading')
+var app = new Vue({
+    el: '#app',
+    data: {
+        formats: [
+            'jpeg',
+            'png',
+            'tiff',
+            'gif',
+            'bmp',
+        ],
+        currentFormat: 0,
 
-let input = document.querySelector('.file-input');
-input.addEventListener('change', (e) => {
-
-    (async () => {
-        let files = await File.fromHtmlFileInputElement(input);
-        if (files.length > 0) {
-
-            let file = files[0];
-    
-            let fnFull = file.name;
-            let fnName = file.name.split('.')[0];
-            let fnExt = file.name.split('.').pop();
-            
-            const result = await main({
-                debug: true,
-                command: `convert '${fnFull}' -quality 1 '${fnName}-out.jpeg'`,
-                inputFiles: [file]
-            })
-            
-            let blob = new Blob([result.outputFiles[0].content.buffer], { type: 'image/png' });
-            document.querySelector('.img-preview').src = URL.createObjectURL(blob);
+        resultImageUrl: '',
+        newFileName: '',
+        quality: 90,
+    },
+    computed: {
+        format() {
+            return this.formats[this.currentFormat];
         }
-        
-    })()
+    },
+    methods: {
+        inputFileEvent(e) {
+            (async () => {
+                let files = await File.fromHtmlFileInputElement(e.target);
+                if (files.length > 0) {
 
+                    let file = files[0];
+
+                    let fnFull = file.name;
+                    let fnName = file.name.split('.')[0];
+                    let fnExt = file.name.split('.').pop();
+
+                    this.newFileName = `${fnName}-out.${this.format}`;
+
+                    let command = '';
+                    if (this.currentFormat == 0) {
+                        command = `convert '${fnFull}' -quality ${this.quality} '${this.newFileName}'`;
+                    } else {
+                        command = `convert '${fnFull}' '${this.newFileName}'`;
+                    }
+
+                    const result = await main({
+                        debug: true,
+                        command: command,
+                        inputFiles: [file]
+                    })
+
+                    let blob = new Blob([result.outputFiles[0].content.buffer], { type: `image/${this.format}` });
+                    this.resultImageUrl = URL.createObjectURL(blob);
+                }
+
+            })()
+        }
+    }
 })
 
